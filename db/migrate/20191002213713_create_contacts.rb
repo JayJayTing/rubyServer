05a3f871a3b147bee8a23ab118b87cd38fd35b8b
JayJayTing <tingjayj@gmail.com>
2019-10-02 20:30:18 -0400

    done

--- db/migrate/20191002213713_create_contacts.rb
+++ db/migrate/20191002213713_create_contacts.rb
@@ -1,12 +1,9 @@
 class CreateContacts < ActiveRecord::Migration[6.0]
   def change
     
-    add_column :contacts, :first_name, :string
-    add_column :contacts, :last_name, :string
-    add_column :contacts, :email, :string
-    add_column :contacts, :gender, :string
-    add_column :contacts, :avatar, :string
-    add_column :contacts, :phone_number, :string
+    create_table :contacts do |t|
+      t.timestamps
+    end
     
   end
 end
